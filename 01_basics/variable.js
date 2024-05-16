e587daf31e8f32436fbab55d451a0cb908fe31e8
Ashish Pal <118504077+AshishPal8@users.noreply.github.com>
2024-05-16 17:18:52 +0530

    learn about variables

---
+++ 01_basics/variable.js
@@ -0,0 +1,18 @@
+const accountId = 1010;
+let accountEmail = "asish@gmail.com";
+var accountPassword = "121212";
+let accountCity = "Ghaziabad";
+let accountState;
+
+// accountId = 1020;
+accountEmail = "a@gmail.com";
+accountPassword = "13131313";
+accountCity = "Noida";
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
