6036221a5d65c958c548f10207eedf9dd8fd0384
Ashish Pal <118504077+AshishPal8@users.noreply.github.com>
2024-05-19 15:49:20 +0530

    comparison operators

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -2,6 +2,26 @@ let score = null;
 
 let convertIntoNumber = Number(score);
 
-console.log(typeof score);
-console.log(typeof convertIntoNumber);
-console.log(convertIntoNumber);
+// console.log(typeof score);
+// console.log(typeof convertIntoNumber);
+// console.log(convertIntoNumber);
+
+// ************************operations********************************
+
+let value = 3;
+let subValue = -value;
+// console.log(subValue);
+
+// console.log("3" + 2); //32
+// console.log(3 + "2"); //32
+// console.log(2 + "3" + 2); //232
+// console.log("2" + 3 + 2); //232
+// console.log(2 + 3 + "2"); //52
+
+// console.log(+true);
+
+let x = 3;
+let y = ++x;
+
+console.log(`x: ${x}`);
+console.log(`y: ${y}`);
