69bad4451285bcf454a02446e18670decabb7d0f
Ashish Pal <118504077+AshishPal8@users.noreply.github.com>
2024-05-19 16:57:42 +0530

    datatypes summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,25 @@
+// primitive datatypes
+
+// types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+// console.log(id === anotherId);
+
+// reference ( non primitive)
+
+// Array, Object, Function
+
+const superHeros = ["Saktiman", "Batman", "Superman"];
+
+const myObj = {
+  name: "Ashish",
+  age: 21,
+};
+
+const myFunction = function () {
+  console.log("Hello World");
+};
+
+console.log(typeof anotherId);
