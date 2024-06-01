8ee23df5ae1df65eb60a1a61100d6d9d73ed8323
Ashish Pal <118504077+AshishPal8@users.noreply.github.com>
2024-06-01 20:33:11 +0530

    color changer in js

---
+++ 12_unlimitedColors/script.js
@@ -0,0 +1,28 @@
+const randomNumber = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+let intervalId;
+
+const startChangingColor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+  }
+
+  function changeBgColor() {
+    document.body.style.backgroundColor = randomNumber();
+    document.body.style.transition = "background-color 0.5s ease-in-out";
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+document.querySelector("#start").addEventListener("click", startChangingColor);
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
