c304c19ce6cfbedf9eb583ee9d102d6835cff826
Ashish Pal <118504077+AshishPal8@users.noreply.github.com>
2024-06-21 15:49:22 +0530

    create testimonial slider

---
+++ 18_testimonial-slider/data.js
@@ -0,0 +1,25 @@
+const testimonials = [
+  {
+    name: "Anjali Sharma",
+    photoUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXzj2m5IjEdvPsRJFdaW8VjVxeHwZ8pVA-tQ&s",
+    description:
+      "Participating in the International Yoga Day event led by PM Modi in Srinagar was a memorable experience. It emphasized the importance of yoga in our daily lives and promoted a sense of unity and well-being.",
+  },
+  {
+    name: "Rahul Kapoor",
+    photoUrl:
+      "https://media.istockphoto.com/id/1363118407/photo/portrait-of-young-businessman.jpg?s=612x612&w=0&k=20&c=e9xjo1AdlIbr7ttZe3iBG3CBWKUBwdTMLkPus9DxA_s=",
+    description:
+      "Attending the yoga session with PM Modi at Sher-i-Kashmir International Conference Centre was inspiring. His commitment to promoting yoga globally is commendable, and it was an honor to be part of such a significant event.",
+  },
+  {
+    name: "Priya Singh",
+    photoUrl:
+      "https://cdn2.stylecraze.com/wp-content/uploads/2013/12/10-Most-Powerful-Indian-Business-Women.jpg.webp",
+    description:
+      "The arrangements made by the local administration for PM Modi's visit to Jammu and Kashmir were commendable. The yoga event highlighted the cultural richness of India and the benefits of yoga for physical and mental health.",
+  },
+];
+
+export default testimonials;
---
+++ 18_testimonial-slider/script.js
@@ -0,0 +1,34 @@
+import testimonials from "./data.js";
+
+const photoUrlEl = document.getElementById("photoUrl");
+const descriptionEl = document.getElementById("description");
+const nameEl = document.getElementById("name");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+
+let currentIndex = 0;
+
+function updataTestimonial(i) {
+  const testimonial = testimonials[i];
+  photoUrlEl.src = testimonial.photoUrl;
+  descriptionEl.textContent = testimonial.description;
+  nameEl.textContent = testimonial.name;
+}
+
+function nextTestimonial() {
+  currentIndex = (currentIndex + 1) % testimonials.length;
+  updataTestimonial(currentIndex);
+}
+
+updataTestimonial(currentIndex);
+setInterval(nextTestimonial, 5000);
+
+leftBtn.addEventListener("click", function () {
+  currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
+  updataTestimonial(currentIndex);
+});
+
+rightBtn.addEventListener("click", function () {
+  currentIndex = (currentIndex + 1) % testimonials.length;
+  updataTestimonial(currentIndex);
+});
